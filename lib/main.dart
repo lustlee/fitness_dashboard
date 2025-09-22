d9b67c8e5a694d2fec04706f1b862073c8bcaaf4
Ramazan Tokoshev <r.tokoshev@gmail.com>
2025-09-22 15:18:07 +0600

    created the initial screen folders and widgets

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:fitness_dashboard/const/constant.dart';
+import 'package:fitness_dashboard/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dashboard UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: backgroundColor,
+        brightness: Brightness.dark,
+      ),
+      home: const MainScreen(
+        title: 'Flutter Demo Home Page',
+      ),
+    );
+  }
+}
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,38 @@
+import 'package:fitness_dashboard/widgets/side_menu_widget.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  final String title;
+
+  const MainScreen({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: SizedBox(
+                child: SideMenuWidget(),
+              ),
+            ),
+            Expanded(
+              flex: 7,
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(
+                color: Colors.green,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
